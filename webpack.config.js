5c76d22a57bd38b416a1742243cc22a27250d9cc
Timofei Larin <timofeilarine31@gmail.ru>
2022-01-18 21:19:55 +0300

    webpack.config.js edited: add pug

--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,96 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPugPlugin = require("html-webpack-pug-plugin");
 
-let mode = 'development'
-if (process.env.NODE_ENV === 'production') {
-    mode = 'production'
+let mode = "development";
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
 }
-console.log(mode + ' mode')
+console.log(mode + " mode");
 
 module.exports = {
-    mode: mode,
-    entry: {
-        scripts: './src/index.js'
+  mode: mode,
+  entry: {
+    scripts: "./src/index.js",
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    assetModuleFilename: "assets/[hash][ext][query]",
+    clean: true,
+  },
+  devtool: "source-map",
+  optimization: {
+    splitChunks: {
+      chunks: "all",
     },
-    output: {
-        filename: '[name].[contenthash].js',
-        assetModuleFilename: "assets/[hash][ext][query]",
-        clean: true,
-    },
-    devtool: 'source-map',
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-        },
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css'
-        }),
-        new HtmlWebpackPlugin({
-            template: "./src/index.pug"
-        })],
-    module: {
-        rules: [
-            {
-            test: /\.html$/i,
-            loader: "html-loader",
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    (mode === 'development') ? "style-loader" : MiniCssExtractPlugin.loader,
-                    "css-loader",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pug/pages/index.pug",
+      filename: "./index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pug/pages/index2.pug",
+      filename: "./index2.html",
+    }),
+    new HtmlWebpackPugPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          mode === "development" ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
                     {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    [
-                                        "postcss-preset-env",
-                                        {
-                                            // Options
-                                        },
-                                    ],
-                                ],
-                            },
-                        },
+                      // Options
                     },
-                    "sass-loader",
+                  ],
                 ],
+              },
             },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader',
-                exclude: /(node_modules|bower_components)/,
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    },
-}
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.pug$/,
+        loader: "@webdiscus/pug-loader",
+        exclude: /(node_modules|bower_components)/,
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
